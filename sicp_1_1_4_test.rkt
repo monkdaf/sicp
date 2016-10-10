1354c6a4b83986201e545d47e79cae4709c19522
Dmitry Fil <monkdaf@gmail.com>
2016-10-10 14:41:28 +0300

    chapter 1.1.4

---
+++ sicp_1_1_4_test.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+(define (square x) (* x x))
+
+(square 2)
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(sum-of-squares 3 4)
+
+(define (f a)
+  (sum-of-squares (+ a 1) (* a 2)))
+
+(f 5)
